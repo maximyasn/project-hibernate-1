72d431b1d4bcf405ac1632cf82c9e65fd987f8ad
maximyasn <maximyasniy11@gmail.com>
2023-09-06 13:41:05 +0300

    Initial commit, add two dependencies into pom.xml

---
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -0,0 +1,48 @@
+package com.game.repository;
+
+import com.game.entity.Player;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository(value = "db")
+public class PlayerRepositoryDB implements IPlayerRepository {
+
+    public PlayerRepositoryDB() {
+
+    }
+
+    @Override
+    public List<Player> getAll(int pageNumber, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public int getAllCount() {
+        return 0;
+    }
+
+    @Override
+    public Player save(Player player) {
+        return null;
+    }
+
+    @Override
+    public Player update(Player player) {
+        return null;
+    }
+
+    @Override
+    public Optional<Player> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void delete(Player player) {
+
+    }
+
+
+}
